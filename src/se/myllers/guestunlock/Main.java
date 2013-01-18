67ce78784131d364eeefa279dd5fcf71cefbc5a8
Mylleranton <anton.moller@hotmail.com>
2013-01-18 22:33:46 +0100

    Added GPLv3 + version 3.4.0

--- src/se/myllers/guestunlock/Main.java
+++ src/se/myllers/guestunlock/Main.java
@@ -1,6 +1,6 @@
 /*
  * GuestUnlock - a bukkit plugin
- * Copyright (C) 2012 Mylleranton
+ * Copyright (C) 2013 Mylleranton
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
